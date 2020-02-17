eaeebcf518582d4e01fd3baf63ac90876cac766a
Alexander <info@alexandertodorov.com>
2020-02-18 00:02:08 +0200

    readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # demo
 A simple demo project
+
+##Installation Instructions
+
+ 1. ```git clone https://github.com/alextodorov/demo.git```
+ 2. ```cd demo```
+ 3. ```composer install```
+ 
+ ##Run
+ 
+ 1. ```php -S localhost:8080 -t public/```
+ 
+ ###Requests
+ 1. Get access token:
+ ```curl -X POST http://localhost:8080/get-token -i -H 'Content-type: application/json' -H 'Authorization: Bearer c2VjcmV0X3VzZXI6JnNkam8x'```
+ 
+ 2. Make request:
+ ```curl -X POST http://localhost:8080/ -i -H 'Content-type: application/json' -H 'Authorization: Bearer yourToken'  -d '{"0":"ZA", "1":"ZA"}'```
+ 
--- config/config.php
+++ config/config.php
@@ -13,14 +13,10 @@ return [
     'auth' => [
         'credentials' => [
             'user' => 'secret_user',
-            'pass' => '&sdjo1', //c2VjcmV0X3VzZXI6JnNkam8x
+            'pass' => '&sdjo1',
         ],
         'api_key' => 'access-token-api',
         'secret_key' => 'just_key',
         'vector' => 'new_vector',
     ],
 ];
-//curl -X POST http://localhost:9999/get-token -i -H 'Content-type: application/json' -H 'Authorization: Bearer c2VjcmV0X3VzZXI6JnNkam8x'
-//eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJsb2NhbGhvc3QiLCJ1c2VyIjp0cnVlLCJleHAiOjE1ODQ2NTIyNTl9.WUVPakg4OVVoOXBmelg4Q3U5ZHB6THZnSlRvRnNLK3A4UXFRNFlXQmg1R05jak51UHpENTQ1N1B2Q3E3NkRxWWpheWNwM2ZpU2wzZ2V4MFBOYWJtcnhHVGtBYWhSVHhUaFlKQUtnSGJZV0xTczlVZkVPMkFDalc2RVlIR1ZqYzZWTVNINlhYQmVYMDRPOUp1VytUQVZRPT0=
-
-//curl -X POST http://localhost:9999/ -i -H 'Content-type: application/json' -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJsb2NhbGhvc3QiLCJ1c2VyIjp0cnVlLCJleHAiOjE1ODQ2NTIyNTl9.WUVPakg4OVVoOXBmelg4Q3U5ZHB6THZnSlRvRnNLK3A4UXFRNFlXQmg1R05jak51UHpENTQ1N1B2Q3E3NkRxWWpheWNwM2ZpU2wzZ2V4MFBOYWJtcnhHVGtBYWhSVHhUaFlKQUtnSGJZV0xTczlVZkVPMkFDalc2RVlIR1ZqYzZWTVNINlhYQmVYMDRPOUp1VytUQVZRPT0='  -d '["ZA", "ZA"]'
